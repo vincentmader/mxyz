28d5cccee14d4c0e43570f37529567608b1e5233
Vincent C. Mader <vincent.mader@icloud.com>
2022-05-05 23:34:03 +0200

    created basic Rocket server

--- mxyz-server/src/main.rs
+++ mxyz-server/src/main.rs
@@ -1,3 +1,31 @@
-pub fn main() {
-    println!("\nHello, mxyz-server! :)")
+#[macro_use]
+extern crate rocket;
+
+use rocket::fs::{relative, FileServer};
+use rocket_dyn_templates::Template;
+
+// use rocket::response::stream::Event;
+// use rocket::tokio::time::Duration;
+
+// use mxyz_server::events;
+// use mxyz_server::views;
+// use mxyz_server;
+// mod views;
+// use mxyz_server::run_server;
+
+#[rocket::main]
+pub async fn main() -> Result<(), rocket::Error> {
+    let routes = routes![
+        // views::index::route,
+        // views::simulation::route,
+        // views::chronos::route,
+    ];
+    let file_server = FileServer::from(relative!("static"));
+
+    rocket::build()
+        .mount("/", routes)
+        .mount("/static", file_server)
+        .attach(Template::fairing())
+        .launch()
+        .await
 }
