9d381497794dc123b1c3a7c05511425db1b2beb5
Vincent C. Mader <vincent.mader@icloud.com>
2022-06-17 00:17:54 +0200

    moved database to separate crate (accessible from both server & engine)

--- mxyz-server/src/main.rs
+++ mxyz-server/src/main.rs
@@ -2,19 +2,29 @@
 extern crate rocket;
 
 use rocket::fs::{relative, FileServer};
+use rocket::response::content;
 use rocket_dyn_templates::Template;
 mod views;
 
-use rocket::response::content;
-#[get("/test")]
-fn json() -> content::Json<&'static str> {
+#[get("/test_json")]
+fn test_json() -> content::Json<&'static str> {
     let foo = "{ 'hi': 'world' }"; // TODO get from engine's state-vec (or db?)
+    content::Json(foo)
+}
+#[get("/test_bytes")]
+fn test_bytes() -> content::Json<&'static str> {
+    let foo = "{}"; // TODO bytes!
     content::Json(foo)
 }
 
 #[rocket::main]
 pub async fn main() -> Result<(), rocket::Error> {
-    let routes = routes![views::index::route, views::simulation::route, json];
+    let routes = routes![
+        views::index::route,
+        views::simulation::route,
+        test_json,
+        test_bytes
+    ];
     let catchers = catchers![views::error::not_found::route,];
     let file_server = FileServer::from(relative!("static"));
 
