8bb40a3c2c67b75f46142a203dc3202f0a1f6874
Vincent C. Mader <vincent.mader@icloud.com>
2022-06-17 23:09:53 +0200

    added scroll-view to big-screen simulation page

--- mxyz-server/src/routes/simulation/mod.rs
+++ mxyz-server/src/routes/simulation/mod.rs
@@ -5,7 +5,7 @@ use rocket_dyn_templates::Template;
 
 #[get("/simulations/<category>/<sim_id>")]
 pub fn route(category: &str, sim_id: &str) -> Template {
-    let title = get_title_from_sim_id(sim_id);
+    let title = get_title_from_sim_id(category, sim_id);
     let context: HashMap<&str, &str> = [
         ("category", category),
         ("sim_id", sim_id),
@@ -17,10 +17,17 @@ pub fn route(category: &str, sim_id: &str) -> Template {
     Template::render("simulation/base", &context)
 }
 
-pub fn get_title_from_sim_id(sim_id: &str) -> String {
-    match sim_id {
-        "3body-moon" => "Star-Planet-Moon System",
-        _ => sim_id, // _ => todo!("TODO: define title for \"{}\"", sim_id),
+pub fn get_title_from_sim_id(category: &str, sim_id: &str) -> String {
+    match category {
+        "gravity" => match sim_id {
+            "3body-moon" => "3-BODY SYSTEM: STAR - PLANET - MOON",
+            _ => todo!("TODO: define title for simulation-id \"{}\"", sim_id),
+        },
+        "oscillators" => match sim_id {
+            "double-pendulum" => "DOUBLE PENDULUM",
+            _ => todo!("TODO: define title for simulation-id \"{}\"", sim_id),
+        },
+        _ => todo!("TODO: define title for category \"{}\"", category),
     }
     .to_string()
 }
