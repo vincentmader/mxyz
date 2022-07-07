c035687bfa53564d505293d8a48786bc689b0f81
Vincent C. Mader <vincent.mader@icloud.com>
2022-07-07 12:16:42 +0200

    removed mxyz-engine submodule and re-created as crate in this repo

---
+++ mxyz-engine/src/lib.rs
@@ -0,0 +1,16 @@
+// #![allow(unused_doc_comments)]
+// // #![allow(unused_variables)]
+// // #![allow(unused_imports)]
+// // #![allow(dead_code)]
+// // #![allow(unreachable_patterns)]
+// // #![allow(non_snake_case)]
+
+// pub mod config;
+// pub mod engine;
+// pub mod integrator;
+// pub mod interaction;
+// pub mod neighborhood;
+// // pub mod state;
+// pub mod tmp;
+
+// pub use engine::Engine;
