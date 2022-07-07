9cf150fafcb44ef6ad764a9f97823010fdcfc97f
Vincent C. Mader <vincent.mader@icloud.com>
2022-07-07 16:28:59 +0200

    minor

--- mxyz-engine/src/preset/three_body_moon/mod.rs
+++ mxyz-engine/src/preset/three_body_moon/mod.rs
@@ -1,8 +1,9 @@
 #![allow(unused_variables)]
 use mxyz_config::EngineConfig;
-use mxyz_universe::entity;
-use mxyz_universe::system::planets::Planets;
-use mxyz_universe::system::{System, SystemVariant};
+use mxyz_universe::system::System;
+use mxyz_universe::system::SystemVariant;
+// use mxyz_universe::entity;
+// use mxyz_universe::system::planets::Planets;
 // use crate::integrator::Integrator;
 // use mxyz_universe::integrator::Integrator;
 // use mxyz_universe::integrator::IntegratorVariant;
@@ -17,23 +18,23 @@ pub fn preset(systems: &mut Vec<System>, config: &mut EngineConfig) {
     // I. SYSTEMS
     // ========================================================================
     config.step_id.1 = NR_OF_STEPS;
-    const M: f64 = 1.;
-    const G: f64 = 1.;
-    let r = 2.;
+    // const M: f64 = 1.;
+    // const G: f64 = 1.;
+    // let r = 2.;
 
     // System 0: Objects
     // ------------------------------------------------------------------------
-    let system_id = 0;
-    let mut system = Planets::new();
-    let speed = (G * M / r).powf(0.5);
-    for entity_id in 0..2 {
-        let m = M;
-        let x = [r * (entity_id as f64 - 0.5), 0., 0.];
-        let v = [0., speed * (2. * entity_id as f64 - 1.), 0.];
-        let entity = entity::object::planet::Planet::new(m, x, v);
-        system.entities.push(entity);
-    }
-    let variant = SystemVariant::Planets(system);
+    // let system_id = 0;
+    // let mut system = Planets::new();
+    // let speed = (G * M / r).powf(0.5);
+    // for entity_id in 0..2 {
+    //     let m = M;
+    //     let x = [r * (entity_id as f64 - 0.5), 0., 0.];
+    //     let v = [0., speed * (2. * entity_id as f64 - 1.), 0.];
+    //     let entity = entity::object::planet::Planet::new(m, x, v);
+    //     system.entities.push(entity);
+    // }
+    // let variant = SystemVariant::Planets(system);
 
     // II.INTEGRATORS
     // ========================================================================
