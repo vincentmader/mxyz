171de6ad7a790a0609404074b48faf610e1ac328
Vincent C. Mader <vincent.mader@icloud.com>
2022-07-13 23:43:44 +0200

    Revert "Revert "quick-save, not working atm""
    This reverts commit 01d349aba3d0022cd202783e523796ce4be0f058.

--- mxyz-client/src/client.rs
+++ mxyz-client/src/client.rs
@@ -5,6 +5,7 @@ use mxyz_config::ClientConfig;
 use std::cell::RefCell;
 use std::rc::Rc;
 use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
 use wasm_bindgen::prelude::*;
 
 const HOST: &str = "127.0.0.1";
@@ -18,6 +19,7 @@ pub struct SimulationClientV1 {
     websocket: WebSocketClient,
     // TODO add mpsc channel between websocket & config?
 }
+
 #[wasm_bindgen]
 impl SimulationClientV1 {
     /// Creates new Simulation-Renderer-Client
@@ -35,56 +37,69 @@ impl SimulationClientV1 {
             websocket,
         }
     }
+
     /// Initializes Renderer-Client
     pub fn init(&mut self, simulation_variant: &str) {
         dom::set_panic_hook();
         self.renderer.init();
         self.websocket.init().unwrap();
     }
+
     /// Runs Renderer-Client in Animation Loop
-    pub async fn run(mut self) -> Result<(), JsValue> {
-        // ANIMATION LOOP
-        // TODO move to utils/dom/mod.rs (?)
+    pub async fn run(mut self) {
+        let arc = Arc::new(Mutex::new(&mut self));
+        // DB Sync via TCP (WebSocket)
+        let _ = arc.clone().lock().unwrap().foo().await;
+        // Renderer
+        let _ = arc.clone().lock().unwrap().bar().await;
+    }
+
+    async fn foo(&mut self) -> Result<(), JsValue> {
+        let f = || self.get_states().unwrap();
+        self.loopy(0, f).await
+    }
+    async fn bar(&mut self) -> Result<(), JsValue> {
+        let f = || self.display_states().unwrap();
+        self.loopy(1, f).await
+    }
+
+    async fn loopy<F>(&self, r: usize, b: F) -> Result<(), JsValue>
+    where
+        F: FnMut(),
+    {
         let f = Rc::new(RefCell::new(None));
         let g = f.clone();
+        let mut i = 0;
         *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
-            if self.config.frame_id.0 > self.config.frame_id.1 {
-                let _ = f.borrow_mut().take();
-                return;
+            // if self.config.frame_id.0 > self.config.frame_id.1 {
+            //     let _ = f.borrow_mut().take();
+            //     return;
+            // }
+            b();
+            if r == 0 {
+                dom::console_log(&format!("{:?}", i));
+            } else {
+                dom::console_log(&format!("\t\t{:?}", i));
             }
             // std::thread::spawn(|| {});
             // self.step(&tx); //
-            self.step(); //
+            // self.step(); //
+            i += 1;
             dom::request_animation_frame(f.borrow().as_ref().unwrap());
         }) as Box<dyn FnMut()>));
         dom::request_animation_frame(g.borrow().as_ref().unwrap());
         Ok(())
     }
+
+    fn get_states(&self) -> Result<(), JsValue> {
+        Ok(())
+    }
+    fn display_states(&self) -> Result<(), JsValue> {
+        Ok(())
+    }
+
     /// Forwards Renderer to Next Time-Step
-    pub fn step(&mut self) {
-        let frame_id = self.config.frame_id.0;
-        // tmp::draw(i); // TODO create renderer with loop over systems & entities
+    pub fn _step(&mut self) {
         self.config.frame_id.0 += 1;
     }
 }
-
-// fn foo<F>(h: F) -> Result<(), JsValue>
-// where
-//     F: FnMut() -> (),
-// {
-//     let f = Rc::new(RefCell::new(None));
-//     let g = f.clone();
-//     *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
-//         h();
-//         // if self.config.frame_id.0 > self.config.frame_id.1 {
-//         //     let _ = f.borrow_mut().take();
-//         //     return;
-//         // }
-//         // // std::thread::spawn(|| {});
-//         // // self.step(&tx); //
-//         // self.step(); //
-//         dom::request_animation_frame(f.borrow().as_ref().unwrap());
-//     }) as Box<dyn FnMut()>));
-//     dom::request_animation_frame(g.borrow().as_ref().unwrap());
-//     Ok(())
-// }
