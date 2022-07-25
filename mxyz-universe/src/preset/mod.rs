848a0d8e2b0a6e450ebc1d7ebd12a41b98e8a47c
Vincent C. Mader <vincent.mader@icloud.com>
2022-07-25 18:12:23 +0200

    Revert "introduced bug, will revert"
    This reverts commit b0d355322779dc7759a76b17e5afe7d49b6c4902.

--- mxyz-universe/src/preset/mod.rs
+++ mxyz-universe/src/preset/mod.rs
@@ -1,27 +1,5 @@
 #![allow(unreachable_patterns)]
 use serde::{Deserialize, Serialize};
-pub mod three_body_figure_eight;
-pub mod three_body_moon;
-use crate::preset::SimulationVariant;
-use crate::state::State;
-use mxyz_config::EngineConfig;
-
-/// Initialize State & Config
-pub fn initialize(simulation_variant: Option<SimulationVariant>, cfg: &mut EngineConfig) -> State {
-    let mut state = State::new(0);
-    let mut systems = vec![];
-    match simulation_variant {
-        Some(simulation_variant) => match simulation_variant {
-            SimulationVariant::ThreeBodyMoon => {
-                three_body_moon::preset(&mut systems, cfg);
-                state.systems = systems;
-                state
-            }
-            _ => todo!(),
-        },
-        None => todo!("handle this earlier? (in str->enum sim-var conversion)"),
-    }
-}
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub enum SimulationVariant {
