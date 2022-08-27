df7676180d41c69fb4a9ac34c85a9f599b2244c9
Vincent C. Mader <vincent.mader@icloud.com>
2022-08-27 20:07:43 +0200

    cleaned up

--- src/mxyz-server/static/js/simulation/base.js
+++ src/mxyz-server/static/js/simulation/base.js
@@ -4,23 +4,14 @@ import init, {
 } from "../../../static/pkg/mxyz_client.js";
 
 let script = document.getElementById("simulation.js");
+
 let category = script.getAttribute("category");
 let simulation_variant = script.getAttribute("simulation_variant");
 
-console.log(
-  "category-id: \t\t\t" +
-    category +
-    "\nsimulation-variant: \t" +
-    simulation_variant
-);
-
 (async () => {
   await init();
-
-  // Thread pool initialization with the given number of threads
-  // (pass `navigator.hardwareConcurrency` if you want to use all cores).
   // await initThreadPool(navigator.hardwareConcurrency);
 
   let simulation = SimulationClientV1.new();
-  simulation.init(simulation_variant);
+  simulation.init(category, simulation_variant);
 })();
