ee7788a777e2df3038f3d757f36cac82b8d97e1f
Vincent C. Mader <vincent.mader@icloud.com>
2022-08-28 11:59:53 +0200

    cleaned up

--- src/mxyz-engine/src/config/mod.rs
+++ src/mxyz-engine/src/config/mod.rs
@@ -3,35 +3,26 @@ use mxyz_engine_universe::preset::SimulationVariant;
 pub struct EngineConfig {
     pub step_id: (usize, usize),
     pub simulation_variant: Option<SimulationVariant>,
-    // pub systems: Vec<SystemConfig>,
-    // pub interactions: Vec<Interaction>,
-    // pub integrators: Vec<Vec<Integrator>>,
-    // pub constants: Constants,
     pub export_variant: ExportVariant,
     pub last_export_step_id: Option<usize>,
     pub nr_of_steps_between_exports: usize,
+    // pub constants: Constants,
 }
 impl EngineConfig {
     pub fn new() -> Self {
-        // let systems = vec![];
-        // let interactions = vec![];
-        // let integrators = vec![];
         let step_id = (0, usize::MAX);
-        // let constants = Constants::new();
         let export_variant = ExportVariant::ToDatabase;
         let last_export_step_id = None;
         let nr_of_steps_between_exports = 100;
         let simulation_variant = None;
+        // let constants = Constants::new();
         EngineConfig {
-            // systems,
-            // interactions,
-            // integrators,
             step_id,
-            // constants,
             export_variant,
             last_export_step_id,
             nr_of_steps_between_exports,
             simulation_variant,
+            // constants,
         }
     }
 }
