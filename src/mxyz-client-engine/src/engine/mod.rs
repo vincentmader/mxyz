4392c0a7c0b06bfa33854166201eb5ec1e968d34
Vincent C. Mader <vincent.mader@icloud.com>
2022-08-28 12:12:09 +0200

    created workspace crates for server-side & client-side engine computes

---
+++ src/mxyz-client-engine/src/engine/mod.rs
@@ -0,0 +1,5 @@
+use mxyz_engine_universe::state::State;
+
+pub struct SimulationEngineV1Client{
+    states: Vec<State>,
+}
---
+++ src/mxyz-server-engine/src/engine/mod.rs
@@ -0,0 +1,5 @@
+use mxyz_engine_universe::state::State;
+
+pub struct SimulationEngineV2Server {
+    states: Vec<State>,
+}
