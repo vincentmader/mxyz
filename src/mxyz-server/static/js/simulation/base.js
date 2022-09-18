b09da36dc73561b73dee0d9353cb975d4e621d36
Vincent C. Mader <vincent.mader@icloud.com>
2022-09-18 02:07:38 +0200

    minor

--- src/mxyz-server/static/js/simulation/base.js
+++ src/mxyz-server/static/js/simulation/base.js
@@ -1,15 +1,12 @@
 import init, {
-  // initThreadPool,
-  init_client,
+  init_client, // initThreadPool,
 } from "../../../static/pkg/mxyz_client.js";
 
 let script = document.getElementById("simulation.js");
-
 let category = script.getAttribute("category");
 let simulation_variant = script.getAttribute("simulation_variant");
 
 (async () => {
-  await init();
-  // await initThreadPool(navigator.hardwareConcurrency);
+  await init(); // await initThreadPool(navigator.hardwareConcurrency);
   init_client(1, category, simulation_variant);
 })();
